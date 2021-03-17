533e8acf28c278cf05dca3123c9e4f6624f9bc44
newhorizon-tech <alaukikinbox@gmail.com>
2021-03-17 20:07:45 +0530

    Set up webpack

---
+++ dist/main.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log("Javascript works!")
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly93ZWF0aGVyLWFwcC8uL3NyYy9pbmRleC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7OztBQUFBIiwiZmlsZSI6Im1haW4uanMiLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZyhcIkphdmFzY3JpcHQgd29ya3MhXCIpXG4iXSwic291cmNlUm9vdCI6IiJ9
---
+++ src/index.js
@@ -0,0 +1,3 @@
+console.log("Javascript works!")
+
+import './style.scss'
