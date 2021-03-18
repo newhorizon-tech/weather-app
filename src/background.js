e3b8f6fd81f6ad3898318870c44023e978c7849b
newhorizon-tech <alaukikinbox@gmail.com>
2021-03-18 17:11:52 +0530

    Fetch background images

--- src/background.js
+++ src/background.js
@@ -1,1 +1,12 @@
-const 
+import {keys} from '../env'
+
+const getData = async (url) => {
+  const response = await fetch(url);
+  return response.json();
+}
+
+const getBackground = (theme) => {
+  const url = `http://api.giphy.com/v1/stickers/search?q=${theme}&api_key=${keys.gifs}&limit=1&rating=g`
+  const result = getData(url)
+  displayBackground(result)
+}
